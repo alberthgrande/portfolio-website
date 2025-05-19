ba0d7709a6f386e1710a0a9aeadb486b33dba1c2
alberthgrande <alberthgrande98@gmail.com>
2025-05-19 16:13:55 +0800

    added : src/components/Logo.tsx

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo: React.FC = () => {
+  return (
+    <div className="text-2xl font-bold text-white-600 tracking-wide">
+      <span className="text-white bg-stone-950 px-2 py-1 rounded">A</span>
+      <span className="ml-1">G</span>
+      <span className="ml-1">R</span>
+    </div>
+  );
+};
+
+export default Logo;
